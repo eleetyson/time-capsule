c49acf323bcb35ea6b2b4caa45b7afff727b23cc
Ethan <eleetyson@gmail.com>
2020-09-27 09:42:29 -0400

    implemented rollbar

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,4 @@
 class Message < ActiveRecord::Base
   validates :content, presence: true
-  # gem doesn't seem to be working
-  # validates :email_address, presence: true, 'valid_email_2/email': { mx: true, disposable: true, disallow_subaddressing: true }
   validates :email_address, email: true
 end
