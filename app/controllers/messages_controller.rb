5990fe472960468a721065f904cc7604f1717689
Ethan <eleetyson@gmail.com>
2020-09-27 20:32:40 -0400

    adjust time formatting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,9 +5,9 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-
+    
     if @message.save
-      if @message.delivery_time > DateTime.now
+      if @message.delivery_time.utc > DateTime.now.strftime("%a, %e %b %Y %H:%M:%S %z").slice(0..-7).to_datetime.utc
         UserMailer.scheduled_email(@message).deliver_later(wait_until: @message.delivery_time)
         flash[:notice] = "Your message was scheduled :)"
       else
