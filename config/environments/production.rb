c78f9b57dcf1dc7497075b24751f001ba8017f92
Ethan <eleetyson@gmail.com>
2020-10-01 17:40:57 -0400

    update readme

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,6 @@ Rails.application.configure do
   config.action_mailer.default_options = {from: 'noreply.timecapsule@gmail.com'}
   config.action_mailer.default_url_options = {host: 'https://future-me.herokuapp.com'}
 
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
